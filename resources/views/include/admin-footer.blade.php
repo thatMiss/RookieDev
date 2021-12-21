5e5235d82217882a5da231892ce1e530468f8317
Miss <biliyoring@hotmail.com>
2021-12-21 22:28:18 +0200

    added partial views

---
+++ resources/views/include/admin-footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="py-4 bg-light mt-auto">
+    <div class="container-fluid px-4">
+            <div class="text-center text-muted">Copyright &copy; {{date('Y')}} {{config('app.name')}}</div>
+    </div>
+</footer>
---
+++ resources/views/include/footer.blade.php
@@ -0,0 +1,3 @@
+<div class="footer mb-0 d-block">
+    <p>Copyright &copy; {{date('Y')}} {{config('app.name')}}</p>
+</div>
