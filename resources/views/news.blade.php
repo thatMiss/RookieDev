544bdb7e27361224ef793838f61811442561d25a
Miss <biliyoring@hotmail.com>
2021-12-21 22:31:07 +0200

    added new contact and news pages

---
+++ resources/views/news.blade.php
@@ -0,0 +1,67 @@
+<x-guest-layout>
+<style>
+
+.if p{
+
+    color: #fff;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+.if h2{
+    text-align: center;
+}
+
+
+
+
+</style>
+
+<section class="meetings-page" id="meeting">
+    <div class="container if">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="section-heading">
+                    <h2>Latest News</h2>
+                    <img src="assets/images/line-dec.png" alt="">
+                    <p>Nunc urna sem, laoreet ut metus id, aliquet consequat magna. Sed viverra ipsum dolor, ultricies fermentum massa consequat eu.</p>
+                </div><br><br><br>
+            </div>
+       
+            <div class="col-lg-6 templatemo-item-col">
+                <img src="assets/images/single-meeting.jpg">
+                <h4>First </h4>
+                <p>Phasellus convallis mauris sed elementum vulputate. Donec posuere leo sed dui eleifend hendrerit. Sed suscipit suscipit erat, sed vehicula ligula. Aliquam ut sem fermentum sem tincidunt lacinia gravida aliquam nunc. Morbi quis erat imperdiet, molestie nunc ut, accumsan diam.</p>
+                <div class="main-button">
+                    <a href="#">View More</a>
+                </div>
+            </div>
+            <div class="col-lg-6 templatemo-item-col">
+                <img src="assets/images/single-meeting.jpg" >
+                <h4>Second </h4>
+                <p>Integer dapibus, est vel dapibus mattis, sem mauris luctus leo, ac pulvinar quam tortor a velit. Praesent ultrices erat ante, in ultricies augue ultricies faucibus. Nam tellus nibh, ullamcorper at mattis non, rhoncus sed massa. Cras quis pulvinar eros. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                <div class="main-button">
+                    <a href="#">View More</a>
+                </div>
+            </div>
+              <div class="col-lg-6 templatemo-item-col">
+                <img src="assets/images/single-meeting.jpg">
+                <h4>Third</h4>
+                <p>Fusce laoreet malesuada rhoncus. Donec ultricies diam tortor, id auctor neque posuere sit amet. Aliquam pharetra, augue vel cursus porta, nisi tortor vulputate sapien, id scelerisque felis magna id felis. Proin neque metus, pellentesque pharetra semper vel, accumsan a neque.</p>
+                <div class="main-button">
+                    <a href="#">View More</a>
+                </div>
+              </div>
+              <div class="col-lg-6 templatemo-item-col">
+                <img src="assets/images/single-meeting.jpg" >
+                <h4>Fourth</h4>
+                <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean ultrices elementum odio ac tempus. Etiam eleifend orci lectus, eget venenatis ipsum commodo et.</p>
+                <div class="main-button">
+                    <a href="#">View More</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+    
+</section>
+</x-guest-layout>
