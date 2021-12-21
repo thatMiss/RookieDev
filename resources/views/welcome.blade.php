784e47e9ff48b88524037a525353ef0fb0e510d9
Miss <biliyoring@hotmail.com>
2021-12-21 22:32:19 +0200

    modified views for layout integration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,76 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-  <head>
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="Template Mo">
-    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900" rel="stylesheet">
-
-    <!-- Bootstrap core CSS -->
-    <link href="{{asset('vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
-
-    <!-- Additional CSS Files -->
-    <link rel="stylesheet" href="{{asset('css/fontawesome.css')}}">
-    <link rel="stylesheet" href="{{asset('css/templatemo-edu-meeting.css')}}">
-    <link rel="stylesheet" href="{{asset('css/owl.css')}}">
-    <link rel="stylesheet" href="{{asset('css/lightbox.css')}}">
-
-  </head>
-
-<body>
-
-  <!-- Sub Header -->
-  <div class="sub-header">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-12 col-sm-4">
-          <div class="text-right right-icons">
-            <ul>
-			  <li><a href="{{route('login')}}">Login</a></li> 
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <!-- ***** Header Area Start ***** -->
-  <header class="header-area header-sticky">
-      <div class="container">
-          <div class="row">
-              <div class="col-12">
-                  <nav class="main-nav">
-                      <!-- ***** Logo Start ***** -->
-                      <a href="index.html" class="logo">
-                          POLICE
-                      </a>
-                      <!-- ***** Logo End ***** -->
-                      <!-- ***** Menu Start ***** -->
-                      <ul class="nav">
-                          <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
-                          <li><a href="criminals.html">Wanted Criminal</a></li>
-						  <li class="has-sub">
-						  <a href="javascript:void(0)">Complaint</a>
-								<ul class="sub-menu">
-								 <li><a href="report_crime.html">report crime</a></li>
-								 <li><a href="report_accident.html">report accident</a></li>
-								 <li><a href="report_missing.html">report missing</a></li>
-								</ul> 
-                          <li class="scroll-to-section"><a href="#contact">Contact Us</a></li> 
-                      </ul>        
-                      <a class='menu-trigger'>
-                          <span>Menu</span>
-                      </a>
-                      <!-- ***** Menu End ***** -->
-                  </nav>
-              </div>
-          </div>
-      </div>
-  </header>
-  <!-- ***** Header Area End ***** -->
+<x-guest-layout>
 
   <!-- ***** Main Banner Area Start ***** -->
   <section class="section main-banner" id="top" data-section="section1">
@@ -85,7 +13,7 @@
                 <div class="caption">
               <h6>POLICE</h6>
               <h2>Our Mission</h2>
-              <p>Protect the American people and uphold the Constitution of the United States</p>
+              <p>Protect the people and uphold the Constitution of the Earth</p>
               <div class="main-button-red">
                   <div class="scroll-to-section"><a href="#contact">Contact Us Now!</a></div>
               </div>
@@ -119,7 +47,7 @@
               </div>
               <div class="down-content">
                 <h4>Our Priorities</h4>
-                <p>Protect the U.S. against foreign intelligence, espionage, and cyber operations.</p>
+                <p>Protect the Earth against foreign intelligence, espionage, aliens and cyber operations.</p>
               </div>
             </div>
             
@@ -231,69 +159,8 @@
         </div>
       </div>
     </div>
-    <div class="footer">
-      <p>Copyright © 2022 RookieDev </p>
-    </div>
+    <x-footer/>
   </section>
 
-  <!-- Scripts -->
-  <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="assets/js/isotope.min.js"></script>
-    <script src="assets/js/owl-carousel.js"></script>
-    <script src="assets/js/lightbox.js"></script>
-    <script src="assets/js/tabs.js"></script>
-    <script src="assets/js/video.js"></script>
-    <script src="assets/js/slick-slider.js"></script>
-    <script src="assets/js/custom.js"></script>
-    <script>
-        //according to loftblog tut
-        $('.nav li:first').addClass('active');
 
-        var showSection = function showSection(section, isAnimate) {
-          var
-          direction = section.replace(/#/, ''),
-          reqSection = $('.section').filter('[data-section="' + direction + '"]'),
-          reqSectionPos = reqSection.offset().top - 0;
-
-          if (isAnimate) {
-            $('body, html').animate({
-              scrollTop: reqSectionPos },
-            800);
-          } else {
-            $('body, html').scrollTop(reqSectionPos);
-          }
-
-        };
-
-        var checkSection = function checkSection() {
-          $('.section').each(function () {
-            var
-            $this = $(this),
-            topEdge = $this.offset().top - 80,
-            bottomEdge = topEdge + $this.height(),
-            wScroll = $(window).scrollTop();
-            if (topEdge < wScroll && bottomEdge > wScroll) {
-              var
-              currentId = $this.data('section'),
-              reqLink = $('a').filter('[href*=\\#' + currentId + ']');
-              reqLink.closest('li').addClass('active').
-              siblings().removeClass('active');
-            }
-          });
-        };
-
-        $('.main-menu, .responsive-menu, .scroll-to-section').on('click', 'a', function (e) {
-          e.preventDefault();
-          showSection($(this).attr('href'), true);
-        });
-
-        $(window).scroll(function () {
-          checkSection();
-        });
-    </script>
-</body>
-
-</body>
-</html>
+</x-guest-layout>
