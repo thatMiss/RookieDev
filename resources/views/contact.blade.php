affb63e555e7b23a872b41355bfff9fad271b248
Miss <biliyoring@hotmail.com>
2022-01-10 17:16:56 +0200

    Modified Contact Blade

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,33 +1,39 @@
 <x-guest-layout>
-<section class="contact-us" id="contact">
+  <section class="contact-us" id="contact">
     <div class="container">
       <div class="row">
         <div class="col-lg-9 align-self-center">
           <div class="row">
             <div class="col-lg-12">
-              <form id="contact" action="" method="post">
+              @if(session('success'))
+              <div class="alert alert-success" role="alert">
+                {{session ('success')}}
+              </div>
+              @endif
+              <form id="contact" action="{{route('contactUs')}}" method="post">
+                @csrf
                 <div class="row">
                   <div class="col-lg-12">
                     <h2>Are You in Danger Send Message Now!</h2>
                   </div>
                   <div class="col-lg-4">
                     <fieldset>
-                      <input name="name" type="text" id="name" placeholder="YOURNAME...*" required="">
+                      <input name="name" type="text" id="name" placeholder="Your Name*" required="">
                     </fieldset>
                   </div>
                   <div class="col-lg-4">
                     <fieldset>
-                    <input name="email" type="text" id="email" pattern="[^ @]*@[^ @]*" placeholder="YOUR EMAIL..." required="">
-                  </fieldset>
+                      <input name="email" type="email" id="email" placeholder="Your Email*" required="">
+                    </fieldset>
                   </div>
                   <div class="col-lg-4">
                     <fieldset>
-                      <input name="subject" type="text" id="subject" placeholder="SUBJECT...*" required="">
+                      <input name="subject" type="text" id="subject" placeholder="Subject*" required="">
                     </fieldset>
                   </div>
                   <div class="col-lg-12">
                     <fieldset>
-                      <textarea name="message" type="text" class="form-control" id="message" placeholder="YOUR MESSAGE..." required=""></textarea>
+                      <textarea name="message" type="text" class="form-control" id="message" placeholder="Your Message*" required=""></textarea>
                     </fieldset>
                   </div>
                   <div class="col-lg-12">
@@ -60,6 +66,6 @@
         </div>
       </div>
     </div>
-    <x-footer/>
+    <x-footer />
   </section>
 </x-guest-layout>
