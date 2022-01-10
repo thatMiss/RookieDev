898756833ad30b0c69dfe1a01ed8d7fcb5f14fa7
Miss <biliyoring@hotmail.com>
2022-01-10 17:15:12 +0200

    Modified Profile show.blade

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -8,39 +8,43 @@
     <div class="container rounded bg-white mt-5 mb-5">
         <div class="row">
             <div class="col-md-3 border-right">
-                <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img class="rounded-circle mt-5" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"><span class="font-weight-bold">Edogaru</span><span class="text-black-50">edogaru@mail.com.my</span><span> </span></div>
+                <div class="d-flex flex-column align-items-center text-center p-3 py-5">
+                    <img class="rounded-circle mt-5" width="150px" src="{{url('storage/'.$currentUser->avatar)}}">
+                    <span class="font-weight-bold mt-3 mb-2">{{$currentUser->name}}</span>
+                    <span class="text-black-50 mb-2">{{$currentUser->email}}</span>
+                    <span class="text-uppercase">{{$currentUser->user_role}}</span>
+                </div>
             </div>
             <div class="col-md-5 border-right">
                 <div class="p-3 py-5">
                     <div class="d-flex justify-content-between align-items-center mb-3">
                         <h4 class="text-right">Profile Settings</h4>
                     </div>
-                    <div class="row mt-2">
-                        <div class="col-md-6"><label class="labels">Name</label><input type="text" class="form-control" placeholder="first name" value=""></div>
-                        <div class="col-md-6"><label class="labels">Surname</label><input type="text" class="form-control" value="" placeholder="surname"></div>
-                    </div>
-                    <div class="row mt-3">
-                        <div class="col-md-12"><label class="labels">Mobile Number</label><input type="text" class="form-control" placeholder="enter phone number" value=""></div>
-                        <div class="col-md-12"><label class="labels">Address Line 1</label><input type="text" class="form-control" placeholder="enter address line 1" value=""></div>
-                        <div class="col-md-12"><label class="labels">Address Line 2</label><input type="text" class="form-control" placeholder="enter address line 2" value=""></div>
-                        <div class="col-md-12"><label class="labels">Postcode</label><input type="text" class="form-control" placeholder="enter address line 2" value=""></div>
-                        <div class="col-md-12"><label class="labels">State</label><input type="text" class="form-control" placeholder="enter address line 2" value=""></div>
-                        <div class="col-md-12"><label class="labels">Area</label><input type="text" class="form-control" placeholder="enter address line 2" value=""></div>
-                        <div class="col-md-12"><label class="labels">Email ID</label><input type="text" class="form-control" placeholder="enter email id" value=""></div>
-                        <div class="col-md-12"><label class="labels">Education</label><input type="text" class="form-control" placeholder="education" value=""></div>
-                    </div>
-                    <div class="row mt-3">
-                        <div class="col-md-6"><label class="labels">Country</label><input type="text" class="form-control" placeholder="country" value=""></div>
-                        <div class="col-md-6"><label class="labels">State/Region</label><input type="text" class="form-control" value="" placeholder="state"></div>
-                    </div>
-                    <div class="mt-5 text-center"><button class="btn btn-primary profile-button" type="button">Save Profile</button></div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="p-3 py-5">
-                    <div class="d-flex justify-content-between align-items-center experience"><span>Edit Experience</span><span class="border px-3 p-1 add-experience"><i class="fa fa-plus"></i>&nbsp;Experience</span></div><br>
-                    <div class="col-md-12"><label class="labels">Experience in Designing</label><input type="text" class="form-control" placeholder="experience" value=""></div> <br>
-                    <div class="col-md-12"><label class="labels">Additional Details</label><input type="text" class="form-control" placeholder="additional details" value=""></div>
+                    @if(session('success'))
+                    <x-alert type="success">{{session('success')}}</x-alert>
+                    @endif
+                    <form action="{{route('profile.update', $currentUser->id)}}" method="POST" enctype="multipart/form-data">
+                        @csrf
+
+                        <div class="row mt-3">
+                            <div class="col-md-12"><label class="labels">Avatar</label>
+                                <input name="adminAvatar" accept="image/png, image/jpg, image/jpeg, image/gif" type="file" class="form-control">
+                            </div>
+
+                            <div class="col-md-12"><label class="labels">Mobile Number</label>
+                                <input name="phone" type="text" class="form-control" value="{{$currentUser->phone_number}}">
+                            </div>
+
+                            <div class="col-md-12"><label class="labels">Address</label>
+                                <input name="address" type="text" class="form-control" placeholder="enter address line 1" value="{{$currentUser->address}}">
+                            </div>
+
+                            <div class="col-md-12"><label class="labels">Area</label>
+                                <input name="area" type="text" class="form-control" placeholder="enter address line 2" value="{{$currentUser->area}}">
+                            </div>
+                        </div>
+                        <div class="mt-5 text-center"><button class="btn btn-success profile-button" type="submit">Save Profile</button></div>
+                    </form>
                 </div>
             </div>
         </div>
