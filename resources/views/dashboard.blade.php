642b37783bf44556e2d960734f3d27ca8cbfa2c9
Miss <biliyoring@hotmail.com>
2022-01-10 17:16:38 +0200

    Modified Dashboard and Welcome Blades

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,37 +7,37 @@
         <div class="row">
             <div class="col-xl-3 col-md-6">
                 <div class="card bg-primary text-white mb-4">
-                    <div class="card-body">User Control</div>
+                    <div class="card-body">Missing Persons</div>
                     <div class="card-footer d-flex align-items-center justify-content-between">
-                        <a class="small text-white stretched-link" href="#">View Details</a>
-                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                        <span class="small text-white stretched-link">Count</span>
+                        <div class="small text-white">{{$missingCount}}</div>
                     </div>
                 </div>
             </div>
             <div class="col-xl-3 col-md-6">
                 <div class="card bg-warning text-white mb-4">
-                    <div class="card-body">Complaint Form</div>
+                    <div class="card-body">Most Wanted Persons</div>
                     <div class="card-footer d-flex align-items-center justify-content-between">
-                        <a class="small text-white stretched-link" href="#">View Details</a>
-                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                        <span class="small text-white stretched-link">Count</span>
+                        <div class="small text-white">{{$wantedCount}}</div>
                     </div>
                 </div>
             </div>
             <div class="col-xl-3 col-md-6">
                 <div class="card bg-success text-white mb-4">
-                    <div class="card-body">Contact Us Control</div>
+                    <div class="card-body">Accident Report</div>
                     <div class="card-footer d-flex align-items-center justify-content-between">
-                        <a class="small text-white stretched-link" href="#">View Details</a>
-                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                        <span class="small text-white stretched-link">Count</span>
+                        <div class="small text-white">{{$accidentCount}}</div>
                     </div>
                 </div>
             </div>
             <div class="col-xl-3 col-md-6">
                 <div class="card bg-danger text-white mb-4">
-                    <div class="card-body">Missing person</div>
+                    <div class="card-body">Crime Report</div>
                     <div class="card-footer d-flex align-items-center justify-content-between">
-                        <a class="small text-white stretched-link" href="#">View Details</a>
-                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                        <span class="small text-white stretched-link">Count</span>
+                        <div class="small text-white">{{$crimeCount}}</div>
                     </div>
                 </div>
             </div>
@@ -62,57 +62,6 @@
                 </div>
             </div>
         </div>
-        <div class="card mb-4">
-            <div class="card-header">
-                <i class="fas fa-table me-1"></i>
-                Wanted List
-            </div>
-            <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
-                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#!">Delete</a></li>
-                        <li><a class="dropdown-item" href="#!">Edit</a></li>
-                        <li>
-                            <hr class="dropdown-divider" />
-                        </li>
-                        <li><a class="dropdown-item" href="#!">View</a></li>
-                    </ul>
-                </li>
-            </ul>
-            <div class="card-body">
-                <table id="datatablesSimple">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Name</th>
-                            <th>Surname</th>
-                            <th>LastSeen</th>
-                            <th>Date Of Birth</th>
-                            <th>Date of registration</th>
-                            <th>Punishment</th>
-                            <th>Reward</th>
-                            <th>List</th>
-                        </tr>
-                    </thead>
-
-                    <tbody>
-                        <tr>
-                            <td>18001196</td>
-                            <td>HAKAN</td>
-                            <td>SYHMUS</td>
-                            <td>HAKKARI</td>
-                            <td>1950</td>
-                            <td>15/11/2021</td>
-                            <td>Terror</td>
-                            <td>500,000$</td>
-                            <td>GRAY</td>
-
-                        </tr>
-
-                    </tbody>
-                </table>
-            </div>
-        </div>
+        
     </div>
 </x-app-layout>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,26 +2,26 @@
 
   <!-- ***** Main Banner Area Start ***** -->
   <section class="section main-banner" id="top" data-section="section1">
-      <video autoplay muted loop id="bg-video">
-          <source src="assets/images/course-video.mp4" type="video/mp4" />
-      </video>
+    <video autoplay muted loop id="bg-video">
+      <source src="assets/images/course-video.mp4" type="video/mp4" />
+    </video>
 
-      <div class="video-overlay header-text">
-          <div class="container">
-            <div class="row">
-              <div class="col-lg-12">
-                <div class="caption">
+    <div class="video-overlay header-text">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="caption">
               <h6>POLICE</h6>
               <h2>Our Mission</h2>
               <p>Protect the people and uphold the Constitution of the Earth</p>
               <div class="main-button-red">
-                  <div class="scroll-to-section"><a href="#contact">Contact Us Now!</a></div>
-              </div>
-          </div>
+                <div class="scroll-to-section"><a href="#contact">Contact Us Now!</a></div>
               </div>
             </div>
           </div>
+        </div>
       </div>
+    </div>
   </section>
   <!-- ***** Main Banner Area End ***** -->
 
@@ -30,58 +30,19 @@
       <div class="row">
         <div class="col-lg-12">
           <div class="owl-service-item owl-carousel">
-          
+            @foreach($missingPersons as $missing)
             <div class="item">
               <div class="icon">
-                <img src="assets/images/service-icon-01.png" alt="">
+                <img src="{{url('storage/'.$missing->attachment)}}" alt="avatar">
               </div>
               <div class="down-content">
-                <h4>Our Vision</h4>
-                <p>Ahead of the threat</p>
+                <h4>{{$missing->missing_first_name}} {{$missing->missing_last_name}}, Age: {{$missing->age}}</h4>
+                <p>{{$missing->description}}</p>
               </div>
             </div>
-            
-            <div class="item">
-              <div class="icon">
-                <img src="assets/images/service-icon-02.png" alt="">
-              </div>
-              <div class="down-content">
-                <h4>Our Priorities</h4>
-                <p>Protect the Earth against foreign intelligence, espionage, aliens and cyber operations.</p>
-              </div>
-            </div>
-            
-            <div class="item">
-              <div class="icon">
-                <img src="assets/images/service-icon-03.png" alt="">
-              </div>
-              <div class="down-content">
-                <h4>Our Locations</h4>
-                <p>We work around the globe. Along with our Headquarters in Washington, D.C., we have 56 field offices located in major cities throughout the U.S</p>
-              </div>
-            </div>
-            
-            <div class="item">
-              <div class="icon">
-                <img src="assets/images/service-icon-02.png" alt="">
-              </div>
-              <div class="down-content">
-                <h4>Our Budget</h4>
-                <p>In fiscal year 2021, our total direct-funded budget is approximately $9.7 billion.</p>
-              </div>
-            </div>
-            
-            <div class="item">
-              <div class="icon">
-                <img src="assets/images/service-icon-03.png" alt="">
-              </div>
-              <div class="down-content">
-                <h4>Our History</h4>
-                <p>The Police was established in 1908. See our History website for more details on our evolution and achievements over the years.</p>
-              </div>
-            </div>
-            
+            @endforeach
           </div>
+          <h4 class="text-white my-5 text-center">Recent Missing Persons</h4><hr class="text-white">
         </div>
       </div>
     </div>
@@ -94,7 +55,7 @@
           <div class="section-heading">
           </div>
         </div>
-    </div>
+      </div>
   </section>
 
 
@@ -111,22 +72,22 @@
                   </div>
                   <div class="col-lg-4">
                     <fieldset>
-                      <input name="name" type="text" id="name" placeholder="YOURNAME...*" required="">
+                      <input name="name" type="text" id="name" placeholder="Your Name*" required="">
                     </fieldset>
                   </div>
                   <div class="col-lg-4">
                     <fieldset>
-                    <input name="email" type="text" id="email" pattern="[^ @]*@[^ @]*" placeholder="YOUR EMAIL..." required="">
-                  </fieldset>
+                      <input name="email" type="text" id="email" pattern="[^ @]*@[^ @]*" placeholder="Your Mail*" required="">
+                    </fieldset>
                   </div>
                   <div class="col-lg-4">
                     <fieldset>
-                      <input name="subject" type="text" id="subject" placeholder="SUBJECT...*" required="">
+                      <input name="subject" type="text" id="subject" placeholder="Subject*" required="">
                     </fieldset>
                   </div>
                   <div class="col-lg-12">
                     <fieldset>
-                      <textarea name="message" type="text" class="form-control" id="message" placeholder="YOUR MESSAGE..." required=""></textarea>
+                      <textarea name="message" type="text" class="form-control" id="message" placeholder="Your Message*" required=""></textarea>
                     </fieldset>
                   </div>
                   <div class="col-lg-12">
@@ -159,7 +120,7 @@
         </div>
       </div>
     </div>
-    <x-footer/>
+    <x-footer />
   </section>
 
 
